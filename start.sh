2bf22c31fabda53a5b1f6e0114966579b3919a12
Asif Nishat <asifnishat@Asifs-MacBook-Air.local>
2023-04-24 02:16:36 +0530

    added start network script

---
+++ start.sh
@@ -0,0 +1,6 @@
+./network.sh down && ./network.sh up createChannel -c loan -s couchdb
+sleep 5
+cd addCim && ./addCim.sh up -c loan
+sleep 6
+cd ..
+echo "---------Added all 3 organizations-----------"
